5ca843a6182f718d880d92994a988436eba1953b
iamGabby <gabiseef@gmail.com>
2020-04-26 20:43:44 -0500

    profilepage

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,17 @@ Vue.use(VueRouter)
     component: function () {
       return import(/* webpackChunkName: "Location" */ '../views/Map.vue')
     }
-  }
+  },
+  {
+    path: '/profile',  
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    componenet: function (){
+      return import(/*"Location" */ '../view/Profile.vue')  
+    }
+  },
 ]
 
 const router = new VueRouter({
